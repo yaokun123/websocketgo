96993876c27638e490e3129a8b2c25ba282a4561
yaok <yaok@pingansec.com>
2019-06-15 12:01:39 +0800

    add

---
+++ chat/impl.go
@@ -0,0 +1,143 @@
+package chat
+
+import (
+	"bytes"
+	"errors"
+	"github.com/gorilla/websocket"
+	"sync"
+)
+
+//封装websocket
+type Connection struct {
+	wsConnect *websocket.Conn
+	inChan chan []byte
+	outChan chan []byte
+	closeChan chan byte
+	cliMap map[string]map[string]*Connection
+	room string
+
+	mutex sync.Mutex	//对closeChan关闭上锁
+	isClosed bool		//防止closeChan被关闭多次
+}
+
+func InitConnection(wsConn *websocket.Conn,cliMap map[string]map[string]*Connection,room string ) (conn *Connection,e error) {
+	conn = &Connection{
+		wsConnect:wsConn,
+		inChan:make(chan []byte,1000),
+		outChan:make(chan []byte,1000),
+		closeChan:make(chan byte,1),
+		cliMap:cliMap,
+		room:room,
+	}
+	
+	//启动读协程
+	go conn.readLoop()
+
+	//启动写协程
+	go conn.writeLoop()
+	
+	return 
+}
+
+func (conn *Connection)ReadMessage() (data []byte,err error) {
+	select {
+	case data = <- conn.inChan:
+	case <- conn.closeChan:
+		err = errors.New("connection is closed")
+	}
+	return
+}
+
+func (conn *Connection)WriteMessage(data []byte) (err error) {
+	select {
+	case conn.outChan <- data:
+	case <- conn.closeChan:
+		err = errors.New("connection is closed")
+	}
+	return
+}
+
+func (conn *Connection)Close()  {
+	//线程安全，可以多次调用
+	conn.wsConnect.Close()
+
+	//利用标记，让closeChan只关闭一次
+	conn.mutex.Lock()
+
+	if !conn.isClosed{
+		close(conn.closeChan)
+		conn.isClosed = true
+
+		//告诉大家该用户退出
+		for _,others := range conn.cliMap[conn.room]{
+			if others == conn{
+				continue
+			}
+			select {
+			case others.inChan <- BytesCombine([]byte(conn.wsConnect.RemoteAddr().String()+"退出聊天室")):		//读取到数据之后写入inChan通道中
+				//case val.inChan <- data:		//读取到数据之后写入inChan通道中
+			case <- others.closeChan:
+				others.Close()
+				continue
+			}
+
+		}
+		//将关闭的资源从map中删除
+		delete(conn.cliMap[conn.room],conn.wsConnect.RemoteAddr().String())
+	}
+
+	conn.mutex.Unlock()
+}
+
+//内部实现
+func (conn *Connection)readLoop()  {
+	for  {
+		_,data,err := conn.wsConnect.ReadMessage()		//读取来自websocket协议的信息
+		if err != nil{
+			conn.Close()
+			return
+		}
+
+		//阻塞在这里，等待inChan有空闲位置
+		for _,val := range conn.cliMap[conn.room]{
+			if val == conn{
+				continue
+			}
+			select {
+			case val.inChan <- BytesCombine([]byte(conn.wsConnect.RemoteAddr().String()+"在聊天室["+conn.room+"]对大家说："),data):		//读取到数据之后写入inChan通道中
+			//case val.inChan <- data:		//读取到数据之后写入inChan通道中
+			case <- val.closeChan:
+				val.Close()
+				continue
+			}
+		}
+
+	}
+}
+
+func BytesCombine(pBytes ...[]byte) []byte {
+	return bytes.Join(pBytes, []byte(""))
+}
+
+func (conn *Connection)writeLoop()  {
+	var data []byte
+	for{
+		select {
+		case data = <-conn.outChan:		//读取outChan通道中的信息
+		case <- conn.closeChan:
+			conn.Close()
+			return
+		}
+		err := conn.wsConnect.WriteMessage(websocket.TextMessage,data)		//将读到的信息发送给客户端
+
+		if err != nil{
+			conn.Close()
+			return
+		}
+
+		if string(data) == "otherConnection"{
+			conn.Close()
+			return
+		}
+	}
+}
---
+++ main/server.go
@@ -0,0 +1,168 @@
+package main
+
+import (
+	"github.com/gorilla/websocket"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+	"websocketgo/chat"
+)
+
+var(
+	upgrader = websocket.Upgrader{
+		//允许跨域
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+
+	cliMap = make(map[string]map[string]*chat.Connection)
+
+	ipMap = make(map[string]map[string]string)
+)
+
+
+func wsHandler(w http.ResponseWriter,r *http.Request)  {
+	//w.Write([]byte("hello"))
+	rooms, ok := r.URL.Query()["room"]
+	var room string
+
+	if !ok {
+		room = "1"		//如果传入的房间号不存在，则默认是第一个房间
+	}else{
+		room = rooms[0]
+	}
+
+
+
+	//完成ws协议的握手操作
+	//Upgrade:websocket
+	wsConn,err := upgrader.Upgrade(w,r,nil)
+	if err != nil{
+		return
+	}
+
+	ipAddress := wsConn.RemoteAddr().String() //获取远程客户端地址 127.0.0.1:61798
+
+
+	//查看客户端是否已经在ipMap中，如果在则要通知已经存在的链接断掉
+	ipArr := strings.Split(ipAddress,":")
+	ip := ipArr[0]
+
+	key,ok := ipMap[room][ip]
+	if ok {				//如果已经存在
+		if cliMap[room][key] != nil{
+			cliMap[room][key].WriteMessage([]byte("otherConnection"))//向客户端发送一条约定的断开链接信息
+		}
+		delete(ipMap[room],ip)
+	}
+
+
+	conn,err := chat.InitConnection(wsConn,cliMap,room)
+
+	/*if err != nil{
+		conn.Close()
+		return
+	}*/
+
+
+	//握手完成之后将连接保存到map中(注意使用二维map的规则)
+
+	//这个地方有个bug（进入同一个房间的人会覆盖之前的
+	_,existsCliMap := cliMap[room]
+	if(existsCliMap){
+		  cliMap[room][ipAddress] = conn
+	}else{
+		cliMapTmp := make(map[string]*chat.Connection)
+		cliMapTmp[ipAddress] = conn
+		cliMap[room] = cliMapTmp
+	}
+
+	_,existsIpMap := ipMap[room]
+	if(existsIpMap){
+		ipMap[room][ip] = ipAddress
+	}else{
+		ipMapTmp := make(map[string]string)
+		ipMapTmp[ip] = ipAddress
+		ipMap[room] = ipMapTmp
+	}
+
+
+	//启动协程，不断发送消息
+	/*go func() {
+		for{
+			err2 := conn.WriteMessage([]byte("heartbeat"))
+			if err2 != nil{
+				return
+			}
+			time.Sleep(1*time.Second)
+		}
+	}()*/
+
+	for{
+		data,err3 := conn.ReadMessage()
+		if err3 != nil{
+			conn.Close()
+			return
+		}
+
+		err4 := conn.WriteMessage(data)
+		if err4 != nil{
+			conn.Close()
+			return
+		}
+	}
+
+}
+
+func main()  {
+	http.HandleFunc("/ws",wsHandler)
+	http.HandleFunc("/open",openHandler)
+	http.HandleFunc("/chat",chatHandler)
+	http.ListenAndServe("0.0.0.0:7777",nil)
+}
+
+func openHandler(w http.ResponseWriter,r *http.Request)  {
+	f,err := os.Open("./main/open.html")
+	defer f.Close()
+	if err != nil{
+		w.Write([]byte("file not found"))
+		return
+	}
+	content,err2 := ioutil.ReadAll(f)
+	if err2 != nil{
+		w.Write([]byte("file read fail"))
+		return
+	}
+	w.Write([]byte(content))
+}
+
+func chatHandler(w http.ResponseWriter,r *http.Request)  {
+
+	//f,err := os.Open("/Users/yaok/Applications/Go/src/mygo/main/chat.html")
+	rooms, ok := r.URL.Query()["room"]
+
+	var room string
+	if !ok {
+		room = "1"
+	}else {
+		room = rooms[0]
+	}
+
+	f,err := os.Open("./main/chat.html")
+	defer f.Close()
+	if err != nil{
+		w.Write([]byte("file not found"))
+		return
+	}
+	content,err2 := ioutil.ReadAll(f)
+	if err2 != nil{
+		w.Write([]byte("file read fail"))
+		return
+	}
+
+	//替换content中的{{$wsUri}}
+	content = []byte(strings.Replace(string(content),"{{$wsUri}}","'ws://192.168.0.105:7777/ws?room="+string(room)+"'",-1))
+	w.Write([]byte(content))
+}
